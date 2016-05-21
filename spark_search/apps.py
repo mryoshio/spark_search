ceb7d5b631a2f488423f53dc68d446347b331636
mryoshio <yoshiokaas@gmail.com>
2016-05-21 14:51:35 +0900

    initial commit

---
+++ spark_search/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SparkSearchConfig(AppConfig):
+    name = 'spark_search'
---
+++ spark_search/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Image(models.Model):
+    small = models.CharField(max_length=100);
+    medium = models.CharField(max_length=100);
+    large = models.CharField(max_length=100);
+
+class ProductGroup(models.Model):
+    name = models.CharField(max_length=20);
+
+class Language(models.Model):
+    name = models.CharField(max_length=20);
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=20);
+
+class Item(models.Model):
+    asin = models.CharField(max_length=15);
+    title = models.CharField(max_length=150);
+    binding = models.CharField(max_length=20);
+    salesrank = models.IntegerField(default=0)
+    author = models.CharField(max_length=30);
+    is_adult_product = models.BooleanField(default=False)
+    manufacturer = models.CharField(max_length=20);
+    release_date = models.DateField()
+    product_group = models.ForeignKey(ProductGroup, on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
